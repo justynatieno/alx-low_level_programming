770fe2a561c418c512fe29647daa82d1d3f1d7ef
justine <justineomwandho@gmail.com>
2022-04-14 03:58:32 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_most_numbers prints numbers between 0-9 except 2 and 4
+ * print_most_numbers - prints numbers between 0-9 except 2 and 4
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 : i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;

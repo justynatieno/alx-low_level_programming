332dff30a76dd9df0d47824d9a5ea45f5344fd14
justine <justineomwandho@gmail.com>
2022-04-20 09:27:20 -0700

    fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,7 @@
 void _puts(char *str)
 {
 	while (*str)
-		_puts(*str++);
+		_putchar(*str++);
 
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);

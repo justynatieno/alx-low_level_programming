a736d666ae88354f723e54696850f703be86a115
justine <justineomwandho@gmail.com>
2022-04-20 06:11:09 -0700

    string in reverse and a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+
+	for (c -= 1 ; c >= 0 ; c--)
+		_putchar(s[c]);
+
+	_putchar('\n');
+}

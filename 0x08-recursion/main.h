ebd3e740da672ca1c07055c1950154761090cfc9
justine <justineomwandho@gmail.com>
2022-04-28 08:00:56 -0700

    main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int find_sqrt(int num, int roof);
+int _sqrt_recursion(int n);
+int check_prime(int n, int i);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int check_palindrome(char *s, int len, int index);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int find_strlen(char *s);
+#endif
+

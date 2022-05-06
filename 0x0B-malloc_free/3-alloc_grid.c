2d874148f0ee0d28edad51da98ff3732a3a93d5c
justine <justineomwandho@gmail.com>
2022-05-06 10:15:57 -0700

    2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -38,3 +38,14 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+
+	for (k = 0 ; k < height ; k++)
+	{
+		for (l = 0 ; l < width ; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+
+	return (a);
+}

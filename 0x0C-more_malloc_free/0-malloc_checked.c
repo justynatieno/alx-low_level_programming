6e243e4cffa7e42c2da3e806f5743afaf0c15366
justine <justineomwandho@gmail.com>
2022-05-10 07:14:28 -0700

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: memory to be allocated
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory = malloc(b);
+
+	if (memory == NULL)
+		exit(98);
+
+	return (memory);
+}

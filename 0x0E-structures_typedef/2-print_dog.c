c09cbc75bfafd401c47cddc06015a353cc01624b
Justyna Omwandho <100158474+justynatieno@users.noreply.github.com>
2022-05-12 21:24:32 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,4 +6,21 @@
  * print_dog - prints function struct dog
  * @d: the struct dog to be rinted
  */
+{
+	if (d == NULL)
+		return;
 
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);

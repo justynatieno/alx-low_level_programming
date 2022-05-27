d52a6a48a7e4dbf3c7300d4152d9e31f3ca21472
Justyna Omwandho <100158474+justynatieno@users.noreply.github.com>
2022-05-27 11:01:56 +0300

    sum of all element

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data of a listint_t linked list
+ * @head: pointer to the head node of the listint_t linked list
+ *
+ * Return: the sum of all the data if empty, 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);

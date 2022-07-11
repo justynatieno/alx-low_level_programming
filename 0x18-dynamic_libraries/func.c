f1c742b5658ca7f68cad274d38d3b632daf3f3ba
Justyna <justineomwandho@gmail.com>
2022-07-11 21:36:25 +0300

    functions

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "header.h"
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: value after addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: value after subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: value after multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: value after division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus of a value
+ * @a: first int
+ * @b: second int
+ *
+ * Return: modulus of the integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

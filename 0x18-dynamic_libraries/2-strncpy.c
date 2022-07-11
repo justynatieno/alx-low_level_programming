ed65759a62f50a6e63724f9bbe53821ae7a0e681
Justyna <justineomwandho@gmail.com>
2022-07-11 21:53:02 +0300

    copies string

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: string to store the string copy
+ * @src: the source string
+ * @n: the maximum number of bytes to be copied from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}

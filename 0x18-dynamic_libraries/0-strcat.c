e9cddbc22cd9f81e93f5d6aaa6681728944c9e62
Justyna <justineomwandho@gmail.com>
2022-07-11 21:39:57 +0300

    concanates two strings

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to the string to be concatenated upon
+ * @src: pointer to the string to be concatenated to dest
+ *
+ * Return: the full concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0 ; src[index] ; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
